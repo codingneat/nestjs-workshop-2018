d9f09084685c99ff4971bdde0f8addc22e138ee1
Daniel Vergara <daniel.vergara.echeverri@gmail.com>
2018-09-25 16:41:16 -0500

    Adding mongoose

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './controllers/app.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { BookService } from './services/book.service';
+import { bookSchema } from './schemas/book.schema';
+import { BookController } from './controllers/book.controller';
 
 @Module({
- controllers: [
-     AppController
- ]
+  controllers: [
+    AppController,
+    BookController
+  ],
+  imports: [
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/test', { useNewUrlParser: true }),
+    MongooseModule.forFeature([
+      { name: 'tokenBookSchema', schema: bookSchema }
+    ]),
+  ],
+  providers: [
+    BookService
+  ]
 })
 export class AppModule {}
